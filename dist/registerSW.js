f6398e9fec41f4895648075e2d18711e8c28fa9c
massaparapan <e.ortiz06@ufromail.cl>
2025-11-13 02:41:45 -0300

    feat: update paths for assets

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/massaparapan/sw.js', { scope: '/massaparapan/' })})}
